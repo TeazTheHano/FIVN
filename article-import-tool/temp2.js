794cd699068791a43b4cc81f4fd0de272afb82d9
TeazTheHano <teaz.khuonganhkiet@gmail.com>
2026-03-06 08:47:43 +0700

    minor change

--- article-import-tool/temp2.js
+++ article-import-tool/temp2.js
@@ -1,6 +1,26 @@
 let x = {
-    "CommandName": "UpdateInformationArticleVersion01",
-    "Domain": "Article",
-    "Content": "{\"Id\":\"0b8b9766-96b0-d8ab-df87-2917ab17af06\",\"MerchantId\":\"5845210a-c1f3-4699-b6ad-eefc96d23e8f\",\"Name\":\"Nguyễn Hồng Tâm\",\"SubDescription\":\"Quản lý danh mục đầu tư công nghệ trị giá 50 triệu USD.\",\"Description\":\"<p>https://www.linkedin.com/</p>\\n\",\"FriendlyUrl\":\"mentor-nguyen-hong-tam\",\"MetaName\":\"VinaCapital Ventures\",\"MetaKeyword\":\"12+ Năm\",\"MetaDescription\":\"Investment Director\",\"MetaImage\":\"\",\"Categories\":[{\"Id\":\"e618564c-bdf8-4399-ad7a-5118e6d9b2ba\",\"IsFeatured\":true,\"DisplayOrder\":0}],\"Images\":[{\"Id\":\"20b21c70-d823-4af8-8111-23f884abb371\",\"IsFeatured\":true,\"DisplayOrder\":0}],\"Authors\":[],\"PostTime\":\"2026-02-05T04:19:54.000Z\",\"Files\":[],\"LanguageId\":\"838aef56-78bb-11e6-b5a6-00155d582814\",\"CreatedDate\":\"03-04-2026 08:22:20\",\"CreatedBy\":\"8597b32b-7c62-bea2-11e0-fa185758c3b6\",\"ModifiedDate\":\"03-04-2026 08:22:20\",\"ModifiedBy\":\"8597b32b-7c62-bea2-11e0-fa185758c3b6\",\"CommandInformation\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36\",\"Products\":[]}",
-    "TimeOutSecond": 7
+    "CommandName": "CreateCustomerWithAttributes",
+    
+    "Domain": "User",
+    
+    "Content": `{
+    \"Id\":\"8272e54d-3170-ffe8-3bd1-66998d61953e\",
+    \"MerchantId\":\"5845210a-c1f3-4699-b6ad-eefc96d23e8f\",
+    \"Name\":\"test1\",
+    \"Password\":\"123456\",
+    \"LanguageId\":\"838aef56-78bb-11e6-b5a6-00155d582814\",
+    \"Email\":\"test1@congnghe.co\",
+    \"Mobile\":\"0987654321\",
+    \"SubDescription\":\"https://viod.labo.io/vi-VN\",
+    \"Description\":\"<p>mô tả</p>\",
+    \"AllType\":64,
+    \"Categories\":[],
+    \"Images\":[{\"Id\":\"f3aecd6d-0494-4401-b824-fa392cf9f95d\",
+    \"IsFeatured\":false,
+    \"DisplayOrder\":0}],
+    \"AttributeValues\":[],
+    \"CreatedDate\":\"03-05-2026 10:42:57\",
+    \"CreatedBy\":\"8597b32b-7c62-bea2-11e0-fa185758c3b6\"}`,
+    
+    "TimeOutSecond": 20
 }
